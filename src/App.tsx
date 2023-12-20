50d8f6bc7e80eb668ad5980b9ceafacef25a3309
GShaKi <65718150+GShaKi@users.noreply.github.com>
2023-12-20 22:56:13 +0330

    Fixed some issues in importing and started nabar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+import './../public/stylesheets/App.css'
+import Navbar from './Navbar.tsx'
 
 function App() {
   return (
     <>
-      
+      <Navbar />
+      <div className="hello">no</div>
     </>
   )
 }
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,7 +1,13 @@
+import hamburgerMenuBar from './../public/img/navbar/hamburger-menu-bar-icon.png'
+
 function Navbar() {
     return (
         <>
-            
+            <nav className="flex flex-row">
+                <div className="basis-1/12">
+                    <img src={hamburgerMenuBar} alt="hamburger-menu-bar" />
+                </div>
+            </nav>
         </>
     )
 }
