5586cb0332f9c65b2cb5b74e76d7df8ffb7ec9b5
GShaKi <65718150+GShaKi@users.noreply.github.com>
2024-01-12 01:15:33 +0330

    Imported the first navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'rich-black': '#0D1117',
+        'oxford-blue': '#0F1527',
+        'claret': '#710024',
+        'tomato': '#FA6244',
+        'dark-orange': '#FF8A0E',
+      },
+    },
   },
   plugins: [],
 }
