308874d2e2fd6400250a10dc12b8c1df0603c820
Skilled Cat <97121666+TheSkilledCat@users.noreply.github.com>
2024-01-14 00:04:32 +0330

    Revert "new none select"
    This reverts commit 30aece0efcaedc3fbebc9ad7d6c14247ff0ba3dd.

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -7,17 +7,17 @@ function Sidebar() {
     return (
         <>
             <nav className='z-20 fixed flex flex-col items-center w-1/6 min-w-56 h-full bg-oxford-blue border-r border-gray-1 pt-16'>
-                <div className='h-fit w-full my-2 text-white block mx-4 py-4 pl-10 rounded text-xl duration-200 hover:pl-10 hover:bg-gray-900 select-none'><FontAwesomeIcon icon={faHouse} />  Home</div>
+                <div className='h-fit w-full my-2 text-white block mx-4 py-4 pl-10 rounded text-xl duration-200 hover:pl-10 hover:bg-gray-900'><FontAwesomeIcon icon={faHouse} />  Home</div>
                 <div className='h-fit w-full'>
                     <div className='mb-2 text-gray-1 pl-8 uppercase select-none'>dashboard</div>
                     <ul className='text-white'>
-                        <li className='dashboard-item block mx-4 py-1 pl-8 rounded text-xl transition-all duration-250 hover:pl-10 select-none'><FontAwesomeIcon icon={faBook} />  My Stories</li>
-                        <li className='dashboard-item block mx-4 py-1 pl-8 rounded text-xl transition-all duration-250 hover:pl-10 select-none'><FontAwesomeIcon icon={faPencil} />  New Story</li>
-                        <li className='dashboard-item block mx-4 py-1 pl-8 rounded text-xl transition-all duration-250 hover:pl-10 select-none'><FontAwesomeIcon icon={faGear} />  Settings</li>
+                        <li className='dashboard-item block mx-4 py-1 pl-8 rounded text-xl transition-all duration-250 hover:pl-10'><FontAwesomeIcon icon={faBook} />  My Stories</li>
+                        <li className='dashboard-item block mx-4 py-1 pl-8 rounded text-xl transition-all duration-250 hover:pl-10'><FontAwesomeIcon icon={faPencil} />  New Story</li>
+                        <li className='dashboard-item block mx-4 py-1 pl-8 rounded text-xl transition-all duration-250 hover:pl-10'><FontAwesomeIcon icon={faGear} />  Settings</li>
                     </ul>
                 </div>
                 <div className='h-fit w-full my-6'>
-                    <div className='mb-2 text-gray-1 pl-8 uppercase select-none'>top stories</div>
+                    <div className='mb-2 text-gray-1 pl-8 uppercase'>top stories</div>
                     <ul className='text-white'>
                         <SidebarItem title='Story 1' />
                         <SidebarItem title='Story 2' />
@@ -25,7 +25,7 @@ function Sidebar() {
                     </ul>
                 </div>
                 <div className='h-fit w-full'>
-                    <div className='mb-2 text-gray-1 pl-8 uppercase select-none'>fresh stories</div>
+                    <div className='mb-2 text-gray-1 pl-8 uppercase'>fresh stories</div>
                     <ul className='text-white'>
                         <SidebarItem title='Story 1' />
                         <SidebarItem title='Story 2' />
