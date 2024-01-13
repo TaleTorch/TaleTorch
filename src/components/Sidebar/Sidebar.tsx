fd261dd4c7025f0ad9d4a77cfefd517ee563a196
GShaKi <65718150+GShaKi@users.noreply.github.com>
2024-01-13 21:28:44 +0330

    Fixed fontawesome"

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,14 @@
 import './Sidebar.css'
 import SidebarItem from './SidebarItem'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+
 
 function Sidebar() {
     return (
         <>
             <nav className='z-20 fixed flex flex-col items-center w-1/6 min-w-56 h-full bg-oxford-blue border-r border-claret pt-16'>
-                <div className='h-fit w-full my-2 text-white block mx-4 py-4 pl-10 rounded text-xl duration-200 hover:pl-10 hover:bg-gray-900'>Home</div>
+                <div className='h-fit w-full my-2 text-white block mx-4 py-4 pl-10 rounded text-xl duration-200 hover:pl-10 hover:bg-gray-900'><FontAwesomeIcon icon={faHouse} />Home</div>
                 <div className='h-fit w-full'>
                     <div className='mb-2 text-gray-1 pl-8 uppercase'>Dashboard</div>
                     <ul className='text-white'>
